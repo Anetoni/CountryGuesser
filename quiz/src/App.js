67ef3721a67e5adc73a228a34f63df3bb8dd94f3
Anetoni <aho.anttoni@gmail.com>
2024-02-14 12:59:38 +0200

    retrieved all countries from service + hooks

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,7 +1,23 @@
+import countryService from './services/countries'
+import { useEffect, useState } from 'react'
+
 const App = () => {
+  const [countries, setCountries] = useState([])
+  const [currentCountry, setCurrentCountry] = useState({
+    name: '',
+    flagUrl: ''
+  })
+
+  useEffect(() => {
+    countryService.getAllCountries().then(countries => {
+      setCountries(countries)
+    }
+    )
+  }, [])
+
   return (
     <div className="App">
-
+      
     </div>
   );
 }
