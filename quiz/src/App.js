e8781caa14704b3bc7f38967a96bd029071d553e
Anetoni <aho.anttoni@gmail.com>
2024-02-14 13:18:24 +0200

    testing country component

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,11 +1,12 @@
 import countryService from './services/countries'
 import { useEffect, useState } from 'react'
+import Country from './components/Country'
 
 const App = () => {
   const [countries, setCountries] = useState([])
-  const [currentCountry, setCurrentCountry] = useState({
-    name: '',
-    flagUrl: ''
+  const [randomCountry, setRandomCountry] = useState({
+    name: 'Finland',
+    flagUrl: 'https://flagcdn.com/w320/fi.png'
   })
 
   useEffect(() => {
@@ -17,7 +18,7 @@ const App = () => {
 
   return (
     <div className="App">
-      
+      <Country randomCountry={randomCountry}></Country>
     </div>
   );
 }
