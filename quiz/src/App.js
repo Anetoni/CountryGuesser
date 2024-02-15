7d9efc25f2c43bf5d763a692b459be88b515142b
Anetoni <aho.anttoni@gmail.com>
2024-02-15 22:06:50 +0200

    randomized country + routes implemented

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,25 +1,43 @@
 import countryService from './services/countries'
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Country from './components/Country'
 
 const App = () => {
   const [countries, setCountries] = useState([])
   const [randomCountry, setRandomCountry] = useState({
-    name: 'Finland',
-    flagUrl: 'https://flagcdn.com/w320/fi.png'
+    name: '',
+    flagUrl: ''
   })
 
   useEffect(() => {
     countryService.getAllCountries().then(countries => {
       setCountries(countries)
-    }
+    } 
     )
   }, [])
 
+  const getRandomCountry = () => {
+    const newCountry = countries[Math.floor(Math.random() * countries.length)]
+    setRandomCountry({ name: newCountry.name, flagUrl: newCountry.flags.png })
+    
+    const countriesWithoutCurrentCountry = countries.filter(country => country.name !== newCountry.name)
+    setCountries(countriesWithoutCurrentCountry)
+  }
+
   return (
-    <div className="App">
-      <Country randomCountry={randomCountry}></Country>
-    </div>
+    <Router>
+      <div>
+        <Link className='home-btn' to="/">Home</Link>
+        <Link className='play-btn' onClick={getRandomCountry} to="/quiz">Start playing</Link>
+        <Link className='leaderboard' to="/leaderboard">Leaderboard</Link>
+      </div>
+
+      <Routes>
+        <Route path="/quiz" element={<Country randomCountry={randomCountry}/>} />
+      </Routes>
+    </Router>
+    
   );
 }
 
