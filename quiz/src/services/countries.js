dd5e3720c20436ca0eebb6959a4da2707f6fdfb0
Anetoni <aho.anttoni@gmail.com>
2024-03-08 16:56:58 +0200

    Revert "used api changed"
    This reverts commit c9ed4b5a57841e89db4db4af34a6869b9c514df6.

--- quiz/src/services/countries.js
+++ quiz/src/services/countries.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
-const url = 'https://countriesnow.space/api/v0.1/countries/info?returns=name,flag'
+const urlAll = 'https://restcountries.com/v3.1/all?fields=name,flags'
 
 //Retrieves all countries from the API and returns them to App in JSON-format 
 const getAllCountries = async () => {
-  const request = await fetch(url)
+  const request = await fetch(urlAll)
   const countries = await request.json()
 
-  return countries.data
+  return countries
 }
 
 export default { getAllCountries }
