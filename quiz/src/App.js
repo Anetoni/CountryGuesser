882ad9cd0d80a0181782a0aacf24f227c4b8a36f
Anetoni <aho.anttoni@gmail.com>
2024-03-14 11:40:09 +0200

    multiple functions and states created

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -2,6 +2,7 @@ import countryService from './services/countries'
 import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Quiz from './components/Quiz'
+import AnswerResult from './components/AnswerResult'
 
 const App = () => {
   const [countries, setCountries] = useState([])
@@ -10,6 +11,9 @@ const App = () => {
     flagUrl: ''
   })
   const [userInput, setUserInput] = useState('')
+  const [resultMessage, setResultMessage] = useState(null)
+  const [resultType, setResultType] = useState(null)
+  const [started, setStarted] = useState(false)
 
   useEffect(() => {
     countryService.getAllCountries().then(countries => {
@@ -18,23 +22,39 @@ const App = () => {
     )
   }, [])
 
+  const startQuiz = (event) => {
+    setStarted(true)
+    getRandomCountry(event)
+  }
+
   const checkAnswer = (event) => {
     console.log(randomCountry.name)
-    if (event.target.value.toLowerCase() === randomCountry.name.toLowerCase()) {
-      console.log('CORRECT')
-      const countriesWithoutCurrentCountry = countries.filter(country => country.name !== randomCountry.name)
-      setCountries(countriesWithoutCurrentCountry)
-      getRandomCountry()
-      setUserInput('')
+    if (userInput.toLowerCase() === randomCountry.name.toLowerCase()) {
+      correctAnswer(event)
     }
   }
 
+  const correctAnswer = (event) => {
+    const countriesWithoutCurrentCountry = countries.filter(country => country.name !== randomCountry.name)
+    setUserInput('')
+    setCountries(countriesWithoutCurrentCountry)
+    getRandomCountry(event)
+    setResultType('correct')
+    setResultMessage('Correct! You guessed ' + randomCountry.name)
+    setTimeout(() => {
+      setResultMessage(null)
+    }, 5000)
+  }
+
   const handleInputChange = (event) => {
+    console.log('input changed')
     setUserInput(event.target.value)
   }
 
-  const getRandomCountry = () => {
+  const getRandomCountry = (event) => {
+    event.preventDefault()
     const newCountry = countries[Math.floor(Math.random() * countries.length)]
+    console.log(newCountry)
     setRandomCountry({ name: newCountry.name.common, flagUrl: newCountry.flags.png })
   }
 
@@ -42,12 +62,18 @@ const App = () => {
     <Router>
       <div>
         <Link className='home-btn' to="/">Home</Link>
-        <Link className='play-btn' onClick={getRandomCountry} to="/quiz">Start playing</Link>
+        <Link className='play-btn' to="/quiz">Play</Link>
         <Link className='leaderboard' to="/leaderboard">Leaderboard</Link>
       </div>
 
       <Routes>
-        <Route path="/quiz" element={<Quiz randomCountry={randomCountry} checkAnswer={checkAnswer} userInput={userInput} handleInputChange={handleInputChange}/>} />
+        <Route path="/quiz" element={
+          <div>
+            <Quiz started={started} startQuiz={startQuiz} randomCountry={randomCountry} checkAnswer={checkAnswer} userInput={userInput} handleInputChange={handleInputChange}/>
+            <AnswerResult type={resultType} message={resultMessage}></AnswerResult>
+          </div>
+          } 
+        ></Route>
       </Routes>
     </Router>
     
